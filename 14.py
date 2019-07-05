6a1ba98fefc3f99dc2ecd5f340c2eab68b73fa85
Sai Kumar <52398706+saikumar176@users.noreply.github.com>
2019-07-05 11:54:41 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,7 +1,7 @@
 v=str(input())
 a=v[::-1]
 for i in a:
-  if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+  if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U":
     continue
   else:
     print(i,end="")
